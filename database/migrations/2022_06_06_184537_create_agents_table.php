26338f49395acc28d73ebb7f860ca45f4419d354
PrevailExcel <prevailexcellent@gmail.com>
2022-06-07 22:48:59 +0100

    Created the Ticket Model Added Foreign key to the Agent model migration

--- database/migrations/2022_06_06_184537_create_agents_table.php
+++ database/migrations/2022_06_06_184537_create_agents_table.php
@@ -15,7 +15,16 @@ class CreateAgentsTable extends Migration
     {
         Schema::create('agents', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tenant_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('active')->default(1);
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
         });
     }
 
