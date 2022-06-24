7585b77f4cd1f8899b5ab1b70730b6e8ecc92c08
PrevailExcel <prevailexcellent@gmail.com>
2022-06-24 20:37:23 +0100

    Ticket Migration created

--- database/migrations/2022_06_07_214741_create_tickets_table.php
+++ database/migrations/2022_06_07_214741_create_tickets_table.php
@@ -15,9 +15,19 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->boolean('type')->comment('1 for Private, 0 for Public');
-            $table->boolean('open')->comment('1 for Open, 0 for Closed');
+            $table->unsignedBigInteger('tenant_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->text('message');
+            $table->boolean('type')->comment('1 for Private, 0 for Public')->default(1);
+            $table->boolean('open')->comment('1 for Open, 0 for Closed')->default(1);
+            $table->string('file_1')->nullable();
+            $table->string('file_2')->nullable();
             $table->timestamps();
+
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
         });
     }
 
