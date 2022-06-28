2e5c43657623449d8837b11e8397a88ad8c0b490
PrevailExcel <prevailexcellent@gmail.com>
2022-06-29 00:19:34 +0100

    Final Commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tenant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function tenantLoginPost(Request $request)
+    {
+        if (!Auth::guard('tenant')->attempt($request->only('email', 'password'))) {
+            return redirect()->back()->with('failed', 'Login details do not match');
+        }
+
+        $user = Tenant::where('email', $request['email'])->firstOrFail();
+        $user->createToken('auth_token', ['role:tenant'])->plainTextToken;
+        // return redirect('/tenant');
+    }
+
+    public function agentLoginPost(Request $request)
+    {
+        if (!Auth::guard('agents')->attempt($request->only('email', 'password'))) {
+            return redirect()->back()->with('failed', 'Login details do not match');
+        }
+
+        $user = Tenant::where('email', $request['email'])->firstOrFail();
+        $user->createToken('auth_token', ['role:tenant'])->plainTextToken;
+        // return redirect('/tenant');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,13 @@ Route::get('/', function () {
 
 
 //Email Verification routes
-Route::get('/email/verify', [EmailVerificationController::class, 'show'])->middleware('auth')->name('verification.notice');
- 
-Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
+Route::controller(EmailVerificationController::class)->group(function () {
+    Route::get('/email/verify', 'show')->middleware('auth')->name('verification.notice');
 
-Route::post('/email/verification-notification', [EmailVerificationController::class, 'notify'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+    Route::get('/email/verify/{id}/{hash}', 'verify')->middleware(['auth', 'signed'])->name('verification.verify');
 
+    Route::post('/email/verification-notification', 'notify')->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+});
 
 Route::get('/profile', function () {
     // Only verified users may access this route...
